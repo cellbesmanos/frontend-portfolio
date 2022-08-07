e0c37a528c00129e49ba024239342de3999784bf
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-08-07 15:59:19 +0800

    Feat: Add project route contents

--- src/routes/projects/Projects.js
+++ src/routes/projects/Projects.js
@@ -1,10 +1,67 @@
 import React from "react";
+
+import Socials from "../../components/socials/Socials";
 import "./Projects.css";
 
 export default function Projects() {
   return (
-    <main>
-      <h1>Hello, this is the project page!</h1>
+    <main className="Projects grid-container grid-container--projects">
+      <div>
+        <img
+          src="/assets/images/desktop-preview.jpg"
+          alt="loopstudios desktop preview"
+        />
+      </div>
+
+      <article className="flow">
+        <header className="flow">
+          <h1 className="text-gradient fw-600 fs-600 letter-spacing-1">
+            Loopstudios Landing Page
+          </h1>
+          <p className="letter-spacing-3">
+            A challenge from Frontend Mentor. This projects test someone’s
+            knowledge of fundamental HTML and CSS concepts.
+          </p>
+        </header>
+
+        <section className="flow">
+          <h2 className="uppercase fw-500 letter-spacing-1">
+            Technologies used:
+          </h2>
+
+          <p className="letter-spacing-3">HTML 5, CSS 3, Javascript</p>
+        </section>
+
+        <section className="flow">
+          <h3 className="uppercase text-white fw-600 letter-spacing-1">
+            Highlights
+          </h3>
+
+          <ul className="letter-spacing-3">
+            <li>Lazy loading images through Intersection Observer</li>
+            <li>Handling data from an API asynchronously</li>
+            <li>React Hooks</li>
+          </ul>
+        </section>
+      </article>
+
+      <div
+        className="Projects__controls"
+        aria-label="projects list"
+        role="tablist"
+      >
+        <button role="tab">
+          <span className="sr-only">description here</span>
+        </button>
+        <button role="tab">
+          <span className="sr-only">description here</span>
+        </button>
+        <button role="tab">
+          <span className="sr-only">description here</span>
+        </button>
+      </div>
+
+      <Socials />
     </main>
   );
 }
