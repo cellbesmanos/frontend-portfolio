359a3eb777d2a66d4f8dba8f55e2a2666f64a21b
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-08-07 15:23:12 +0800

    Feat: Add Projects route skeleton

---
+++ src/routes/projects/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Projects.css";
+
+export default function Projects() {
+  return (
+    <main>
+      <h1>Hello, this is the project page!</h1>
+    </main>
+  );
+}
