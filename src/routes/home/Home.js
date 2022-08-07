99060adff82531fd499f3416a9d396e5def182de
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-08-07 15:35:48 +0800

    Fix: Adjust global font size CSS variables
    - Add additional font size for project title
    - Increase size for Home heading on mobile

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
         <header className="text-white uppercase fs-500 fw-500">
           <h1 className="Home__content-heading">
             Hi, I am
-            <span className="block fs-600 fw-600 letter-spacing-2">
+            <span className="block fs-700 fw-600 letter-spacing-2">
               Christian Lhie Besmanos
             </span>
           </h1>
